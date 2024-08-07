3e8e97234778373bf0de4c83025e4c6ea970a202
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-08-07 16:41:56 -0400

    Wrap Prism C API into new `Prism::Parser` class
    And move all other Prism related code to the new parser/prism component

---
+++ parser/prism/Translator.h
@@ -0,0 +1,21 @@
+#ifndef SORBET_PARSER_PRISM_TRANSLATOR_H
+#define SORBET_PARSER_PRISM_TRANSLATOR_H
+
+#include <memory>
+
+#include "../Node.h" // To clarify: these are Sorbet Parser nodes, not Prism ones.
+#include "parser/prism/Parser.h"
+
+extern "C" {
+#include "prism.h"
+}
+
+namespace sorbet::parser::Prism {
+
+class Translator final {
+public:
+    std::unique_ptr<parser::Node> convertPrismToSorbet(pm_node_t *node, Parser parser, core::GlobalState &gs);
+};
+
+} // namespace sorbet::parser::Prism
+#endif // SORBET_PARSER_PRISM_TRANSLATOR_H
