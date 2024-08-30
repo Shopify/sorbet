96577fdb8b971f8894333f0fca913054347407a4
Alexander Momchilov <alexander.momchilov@shopify.com>
2024-08-30 14:05:38 -0400

    wipwipwipw

---
+++ test/prism_regression/assign_to_constant.rb
@@ -0,0 +1,20 @@
+# typed: false
+
+# Regular assignment
+REGULAR = 1
+
+# Compound assignment operators
+BITWISE_AND &= 2
+BITWISE_XOR ^= 4
+SHIFT_RIGHT >>= 5
+SHIFT_LEFT <<= 6
+SUBTRACT_ASSIGN -= 7
+MODULE_ASSIGN %= 8
+BITWISE_OR |= 9
+DIVIDE_ASSIGN /= 10
+MULTIPLY_ASSIGN *= 11
+EXPONENTIATE_ASSIGN **= 12
+
+# Special cases
+LAZY_AND_ASSIGN &&= 13
+LAZY_OR_ASSGIN ||= 14
---
+++ test/prism_regression/assign_to_method_call_result.rb
@@ -0,0 +1,20 @@
+# typed: false
+
+# Regular assignment
+self.regular = 1
+
+# Compound assignment operators
+self.bitwise_and &= 2
+self.bitwise_xor ^= 4
+self.shift_right >>= 5
+self.shift_left <<= 6
+self.subtract_assign -= 7
+self.module_assign %= 8
+self.bitwise_or |= 9
+self.divide_assign /= 10
+self.multiply_assign *= 11
+self.exponentiate_assign **= 12
+
+# Special cases
+self.lazy_and_assign &&= 13
+self.lazy_or_assgin ||= 14
