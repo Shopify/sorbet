1a602a88643475329b8a00fc621a87c48349b863
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-09-17 18:26:46 -0400

    TMP parsing signatures
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- test.rb
+++ test.rb
@@ -2,10 +2,10 @@
 
 extend T::Sig
 
-sig { returns({ id: String, name: String }) }
-def tuple_type0; T.unsafe(nil); end
-T.reveal_type(tuple_type0) # error: Revealed type: `[Integer]`
+sig { params(p1: Integer, p2: T.nilable(String), p3: String, p4: Integer, p5: T.nilable(String), p6: Integer, block: T.proc.void).void }
+def bar(p1, p2 = nil, *p3, p4:, p5: nil, **p6, &block)
+end
 
-#: -> { id: String, name: String }
-def shape_type1; T.unsafe(nil); end
-T.reveal_type(shape_type1) # error: Revealed type: `{ id: String, name: String }`
+#: (Integer, ?String, *String, p4: Integer, ?p5: String, **Integer) { -> void } -> void
+def foo(p1, p2 = nil, *p3, p4:, p5: nil, **p6, &block)
+end
