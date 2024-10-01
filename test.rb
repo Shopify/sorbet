cd01d1b7c4b60233fbd210eea9addc944951d2e4
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2024-10-01 16:33:07 -0400

    WIP: Annotations passing
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- test.rb
+++ test.rb
@@ -25,9 +25,48 @@ extend T::Sig
 # y = nil #: Integer?
 # T.reveal_type(y)
 
-x = "hello" #: String?
+# x = "hello" #: String?
 
-if ARGV.first
-  puts x
-  return x #:: String
-end
+# if ARGV.first
+#   puts x
+#   return x #:: String
+# end
+
+
+# class TypePrinter
+#   extend T::Sig
+
+#   #: String
+#   attr_reader :string
+
+#   #: -> void
+#   def initialize
+#     @string = String.new #: String
+#   end
+# end
+
+# x = 42 #:: untyped
+
+# #: (Array[String?]) -> Array[String]
+# def foo(x)
+#   x.sort do |y|
+#     a = x[0] #:: String
+#     T.reveal_type(a)
+#     b = x[1] #:: String
+#     T.reveal_type(b)
+#     res = a <=> b
+#     T.reveal_type(res)
+#     res
+#   end
+#   x.compact
+# end
+
+# x = ARGV.first
+# case x
+# when "foo"
+#   x #:: String
+# else
+#   x #:: Integer
+# end
+
+x = [1, 2, 3].to_h { |x| [x, "#{x}"] } #: Hash[Integer, String]
