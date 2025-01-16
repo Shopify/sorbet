527e3f9dc56262392bc25eee7f7a72e541252bf2
Alexandre Terrasa <alexandre.terrasa@shopify.com>
2025-01-16 16:33:44 -0500

    Extract `AttrHelper#getName` helper
    Signed-off-by: Alexandre Terrasa <alexandre.terrasa@shopify.com>

--- rewriter/AttrReader.cc
+++ rewriter/AttrReader.cc
@@ -1,5 +1,5 @@
 #include "rewriter/AttrReader.h"
-#include "absl/strings/escaping.h"
+#include "ast/AttrHelper.h"
 #include "ast/Helpers.h"
 #include "ast/ast.h"
 #include "core/Context.h"
@@ -15,54 +15,16 @@ namespace sorbet::rewriter {
 
 namespace {
 
-pair<core::NameRef, core::LocOffsets> getName(core::MutableContext ctx, ast::ExpressionPtr &name) {
-    core::LocOffsets loc;
-    core::NameRef res;
-    if (auto *lit = ast::cast_tree<ast::Literal>(name)) {
-        if (lit->isSymbol()) {
-            res = lit->asSymbol();
-            loc = lit->loc;
-            ENFORCE(ctx.locAt(loc).exists());
-            ENFORCE(ctx.locAt(loc).source(ctx).value().size() > 1 && ctx.locAt(loc).source(ctx).value()[0] == ':');
-            loc = core::LocOffsets{loc.beginPos() + 1, loc.endPos()};
-        } else if (lit->isString()) {
-            core::NameRef nameRef = lit->asString();
-            auto shortName = nameRef.shortName(ctx);
-            bool validAttr = (isalpha(shortName.front()) || shortName.front() == '_') &&
-                             absl::c_all_of(shortName, [](char c) { return isalnum(c) || c == '_'; });
-            if (validAttr) {
-                res = nameRef;
-            } else {
-                if (auto e = ctx.beginIndexerError(name.loc(), core::errors::Rewriter::BadAttrArg)) {
-                    e.setHeader("Bad attribute name \"{}\"", absl::CEscape(shortName));
-                }
-                res = core::Names::empty();
-            }
-            loc = lit->loc;
-            DEBUG_ONLY({
-                auto l = ctx.locAt(loc);
-                ENFORCE(l.exists());
-                auto source = l.source(ctx).value();
-                ENFORCE(source.size() > 2);
-                ENFORCE(source[0] == '"' || source[0] == '\'');
-                auto lastChar = source[source.size() - 1];
-                ENFORCE(lastChar == '"' || lastChar == '\'');
-            });
-            loc = core::LocOffsets{loc.beginPos() + 1, loc.endPos() - 1};
-        }
-    }
-    if (!res.exists()) {
-        if (auto e = ctx.beginIndexerError(name.loc(), core::errors::Rewriter::BadAttrArg)) {
-            e.setHeader("arg must be a Symbol or String");
-        }
-    }
-    return make_pair(res, loc);
-}
-
 // these helpers work on a purely syntactic level. for instance, this function determines if an expression is `T`,
 // either with no scope or with the root scope (i.e. `::T`). this might not actually refer to the `T` that we define for
 // users, but we don't know that information in the Rewriter passes.
 bool isT(const ast::ExpressionPtr &expr) {
+    if (auto *t = ast::cast_tree<ast::ConstantLit>(expr)) {
+        if (t->symbol == core::Symbols::T()) {
+            return true;
+        }
+    }
+
     auto *t = ast::cast_tree<ast::UnresolvedConstantLit>(expr);
     if (t == nullptr || t->cnst != core::Names::Constants::T()) {
         return false;
@@ -236,7 +198,7 @@ vector<ast::ExpressionPtr> AttrReader::run(core::MutableContext ctx, ast::Send *
 
     if (makeReader) {
         for (auto &arg : send->posArgs()) {
-            auto [name, argLoc] = getName(ctx, arg);
+            auto [name, argLoc] = ast::AttrHelper::getName(ctx, arg);
             if (!name.exists()) {
                 return empty;
             }
@@ -259,7 +221,7 @@ vector<ast::ExpressionPtr> AttrReader::run(core::MutableContext ctx, ast::Send *
 
     if (makeWriter) {
         for (auto &arg : send->posArgs()) {
-            auto [name, argLoc] = getName(ctx, arg);
+            auto [name, argLoc] = ast::AttrHelper::getName(ctx, arg);
             if (!name.exists()) {
                 return empty;
             }
