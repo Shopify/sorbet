6556192d14fce81192b0584e584f32e269f1bffe
Kaan Ozkan <kaanozkan97@gmail.com>
2025-04-04 16:00:09 -0400

    Return `parseResult` from `Parser::run`
    `parseResult` includes the comment locations found as well as the root
    node

--- parser/parser.h
+++ parser/parser.h
@@ -23,8 +23,13 @@ public:
         }
     };
 
-    static std::unique_ptr<Node> run(core::GlobalState &gs, core::FileRef file, Settings settings,
-                                     std::vector<std::string> initialLocals = {});
+    struct ParseResult {
+        std::unique_ptr<Node> tree;
+        std::vector<size_t> commentLocations;
+    };
+
+    static ParseResult run(core::GlobalState &gs, core::FileRef file, Settings settings,
+                           std::vector<std::string> initialLocals = {});
 };
 
 } // namespace sorbet::parser
