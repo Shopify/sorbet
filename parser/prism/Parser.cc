bcdf29f03f3978089b5850ffad7e3c6793bad85a
Kaan Ozkan <kaanozkan97@gmail.com>
2025-07-17 10:18:27 -0400

    Collect comments parsed by Prism

--- parser/prism/Parser.cc
+++ parser/prism/Parser.cc
@@ -20,7 +20,7 @@ pm_parser_t *Parser::getRawParserPointer() {
 
 ParseResult Parser::parse() {
     pm_node_t *root = pm_parse(&parser);
-    return ParseResult{*this, root, collectErrors()};
+    return ParseResult{*this, root, collectErrors(), collectCommentLocations()};
 };
 
 core::LocOffsets Parser::translateLocation(pm_location_t location) const {
@@ -56,5 +56,22 @@ vector<ParseError> Parser::collectErrors() {
     }
 
     return parseErrors;
+}
+
+vector<core::LocOffsets> Parser::collectCommentLocations() {
+    vector<core::LocOffsets> commentLocations;
+    commentLocations.reserve(parser.comment_list.size);
+
+    auto commentList = parser.comment_list;
+
+    for (auto *node = commentList.head; node != nullptr; node = node->next) {
+        auto *comment = reinterpret_cast<pm_comment_t *>(node);
+
+        core::LocOffsets location = translateLocation(comment->location);
+
+        commentLocations.push_back(location);
+    }
+
+    return commentLocations;
 }
 }; // namespace sorbet::parser::Prism
