cbef2f57f8c90d5f5b5880f5a072c526bddfc6cf
Kaan Ozkan <kaanozkan97@gmail.com>
2025-07-31 14:14:17 -0400

    Modify endPos of send nodes with blocks to be before the block

--- parser/prism/Translator.cc
+++ parser/prism/Translator.cc
@@ -1703,10 +1703,24 @@ unique_ptr<parser::Node> Translator::translateCallWithBlock(pm_node_t *prismBloc
         body = translate(prismLambdaNode->body);
     }
 
+    auto blockLoc = sendNode->loc;
+
+    // Modify send node's endLoc to be position before first space
+    if (sendNode->loc.exists()) {
+        auto source = file.data(gs).source();
+        auto beginPos = sendNode->loc.beginPos();
+
+        // Find first space character starting from begin position
+        auto spacePos = source.find(' ', beginPos);
+        if (spacePos != std::string_view::npos) {
+            sendNode->loc = core::LocOffsets{beginPos, static_cast<uint32_t>(spacePos)};
+        }
+    }
+
     if (parser::cast_node<parser::NumParams>(parametersNode.get())) {
-        return make_unique<parser::NumBlock>(sendNode->loc, move(sendNode), move(parametersNode), move(body));
+        return make_unique<parser::NumBlock>(blockLoc, move(sendNode), move(parametersNode), move(body));
     } else {
-        return make_unique<parser::Block>(sendNode->loc, move(sendNode), move(parametersNode), move(body));
+        return make_unique<parser::Block>(blockLoc, move(sendNode), move(parametersNode), move(body));
     }
 }
 
