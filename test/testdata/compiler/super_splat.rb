43e8425b49f2be8d97013ea5e29acbd308203ca1
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-09-26 21:19:07 +0300

    wip

--- test/testdata/compiler/super_splat.rb
+++ test/testdata/compiler/super_splat.rb
@@ -2,23 +2,8 @@
 # typed: true
 # frozen_string_literal: true
 
-class Parent
-  extend T::Sig
 
-  def foo(*args)
-    puts args
-  end
+super(*T.unsafe(ARGS))
 
-end
-
-class Child < Parent
-  extend T::Sig
-
-  def foo(*args)
-    super(*T.unsafe(args))
-  end
-
-end
-
-args = [1,2,3]
+ARGS = [1,2,3]
 Child.new.foo(*[1,2,3])
