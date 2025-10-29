15ae32df98b3d8d0772cb50d02468bda6ab22418
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-10-29 09:57:55 -0400

    Fix location of regular Super with a block

--- parser/prism/Translator.cc
+++ parser/prism/Translator.cc
@@ -3309,6 +3309,23 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node, bool preserveCon
             auto blockArgumentNode = superNode->block;
             NodeVec returnValues;
 
+            if (blockArgumentNode) { // Adjust the location to exclude the literal block argument.
+                const uint8_t *start = superNode->base.location.start;
+                const uint8_t *end;
+
+                if (superNode->rparen_loc.end) {
+                    end = superNode->rparen_loc.end;
+                } else if (auto *argP = superNode->arguments) {
+                    auto args = absl::MakeSpan(argP->arguments.nodes, argP->arguments.size);
+                    end = args.back()->location.end;
+                } else {
+                    constexpr auto len = std::size("super") - 1;
+                    end = start + len;
+                }
+
+                location = translateLoc(start, end);
+            }
+
             if (blockArgumentNode != nullptr && PM_NODE_TYPE_P(blockArgumentNode, PM_BLOCK_NODE)) {
                 returnValues = translateArguments(superNode->arguments);
                 auto superNode = make_unique<parser::Super>(location, move(returnValues));
