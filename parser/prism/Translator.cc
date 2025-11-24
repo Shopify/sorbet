06b0b6f27e8df96052258e9de4e8f54b7c370b9c
Jesse Johnson <jesse.johnson@shopify.com>
2025-11-24 15:23:40 -0800

    PM_SYMBOL_NODE return expr_only

--- parser/prism/Translator.cc
+++ parser/prism/Translator.cc
@@ -3447,7 +3447,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node, bool preserveCon
 
             auto [content, location] = translateSymbol(symNode);
 
-            return make_node_with_expr<parser::Symbol>(MK::Symbol(location, content), location, content);
+            return expr_only(MK::Symbol(location, content));
         }
         case PM_TRUE_NODE: { // The `true` keyword
             return expr_only(MK::True(location));
