330e2d0b0b7b6338d4ce97d512bf87728960aaa5
Jesse Johnson <jesse.johnson@shopify.com>
2025-11-24 17:03:35 -0800

    PM_SINGLETON_CLASS_NODE return expr_only

--- parser/prism/Translator.cc
+++ parser/prism/Translator.cc
@@ -3288,8 +3288,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node, bool preserveCon
                 if (auto e = ctx.beginIndexerError(receiver->loc, core::errors::Desugar::InvalidSingletonDef)) {
                     e.setHeader("`{}` is only supported for `{}`", "class << EXPRESSION", "class << self");
                 }
-                auto emptyTree = MK::EmptyTree();
-                return make_node_with_expr<parser::SClass>(move(emptyTree), location, declLoc, move(receiver), nullptr);
+                return expr_only(MK::EmptyTree());
             }
 
             auto bodyExprs = desugarScopeBodyToRHSStore(classNode->body, body);
@@ -3301,7 +3300,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node, bool preserveCon
             auto sClassDef = MK::Class(location, declLoc, move(singletonClassName), ast::ClassDef::ANCESTORS_store{},
                                        move(bodyExprs));
 
-            return make_node_with_expr<parser::SClass>(move(sClassDef), location, declLoc, move(receiver), move(body));
+            return expr_only(move(sClassDef));
         }
         case PM_SOURCE_ENCODING_NODE: { // The `__ENCODING__` keyword
             return make_node_with_expr<parser::EncodingLiteral>(
