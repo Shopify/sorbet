e4da78baeb2aa5cf9f4a40f9407ce890b3ea4e5b
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-12-01 11:42:03 -0500

    Use `Span` in `parseComment()`

--- rbs/prism/AssertionsRewriterPrism.cc
+++ rbs/prism/AssertionsRewriterPrism.cc
@@ -30,7 +30,7 @@ const regex absurd_pattern_prism("^\\s*absurd\\s*(#.*)?$");
  */
 optional<pair<pm_node_t *, InlineCommentPrism::Kind>>
 parseComment(core::MutableContext ctx, parser::Prism::Parser &parser, InlineCommentPrism comment,
-             vector<pair<core::LocOffsets, core::NameRef>> typeParams) {
+             absl::Span<pair<core::LocOffsets, core::NameRef>> typeParams) {
     Factory prism(parser);
 
     if (comment.kind == InlineCommentPrism::Kind::MUST || comment.kind == InlineCommentPrism::Kind::UNSAFE ||
@@ -509,7 +509,7 @@ pm_node_t *AssertionsRewriterPrism::replaceSyntheticBind(pm_node_t *node) {
     auto inlineComment = commentForNode(node);
     ENFORCE(inlineComment, "No inline comment found for synthetic bind");
 
-    auto pair = parseComment(ctx, parser, inlineComment.value(), typeParams);
+    auto pair = parseComment(ctx, parser, inlineComment.value(), absl::MakeSpan(typeParams));
 
     if (!pair) {
         // We already raised an error while parsing the comment, so we just bind to `T.untyped`
@@ -535,7 +535,7 @@ pm_node_t *AssertionsRewriterPrism::maybeInsertCast(pm_node_t *node) {
     }
 
     if (auto inlineComment = commentForNode(node)) {
-        if (auto type = parseComment(ctx, parser, inlineComment.value(), typeParams)) {
+        if (auto type = parseComment(ctx, parser, inlineComment.value(), absl::MakeSpan(typeParams))) {
             return insertCast(node, type);
         }
     }
@@ -573,7 +573,7 @@ void AssertionsRewriterPrism::rewriteNodesAsArray(pm_node_t *node, pm_node_list_
             nodes = (pm_node_list){.size = 1, .capacity = 1, .nodes = buffer};
         }
 
-        if (auto type = parseComment(ctx, parser, inlineComment.value(), typeParams)) {
+        if (auto type = parseComment(ctx, parser, inlineComment.value(), absl::MakeSpan(typeParams))) {
             nodes.nodes[0] = insertCast(nodes.nodes[0], type);
         }
     }
