2737129e6975ead9fc47b244c8e1647d556b2f17
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-12-02 12:31:21 -0500

    Add new `desugarParametersNode()` overload

--- parser/prism/Translator.h
+++ parser/prism/Translator.h
@@ -97,6 +97,9 @@ private:
     std::pair<std::unique_ptr<parser::Params>, core::NameRef /* enclosingBlockParamName */>
     translateParametersNode(pm_parameters_node *paramsNode, core::LocOffsets location);
 
+    std::tuple<ast::MethodDef::PARAMS_store, ast::InsSeq::STATS_store, core::NameRef /* enclosingBlockParamName */>
+    desugarParametersNode(pm_parameters_node *paramsNode, core::LocOffsets location);
+
     std::tuple<ast::MethodDef::PARAMS_store, ast::InsSeq::STATS_store> desugarParametersNode(NodeVec &params);
 
     std::array<core::LocOffsets, 9> findNumberedParamsUsageLocs(core::LocOffsets loc, pm_statements_node *statements,
