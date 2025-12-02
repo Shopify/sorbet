7990babf98206413b5c91a600ee2def99a44c7b4
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-12-02 12:07:29 -0500

    Remove always-true bool return

--- parser/prism/Translator.h
+++ parser/prism/Translator.h
@@ -97,7 +97,7 @@ private:
     std::pair<std::unique_ptr<parser::Params>, core::NameRef /* enclosingBlockParamName */>
     translateParametersNode(pm_parameters_node *paramsNode, core::LocOffsets location);
 
-    std::tuple<ast::MethodDef::PARAMS_store, ast::InsSeq::STATS_store, bool>
+    std::tuple<ast::MethodDef::PARAMS_store, ast::InsSeq::STATS_store>
     desugarParametersNode(NodeVec &params, bool attemptToDesugarParams);
 
     std::array<core::LocOffsets, 9> findNumberedParamsUsageLocs(core::LocOffsets loc, pm_statements_node *statements,
