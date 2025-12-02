a5fd9743598afdd20da9870512f52287fb5c7f5e
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-12-02 11:46:29 -0500

    Move `supportedCallType` down

--- parser/prism/Translator.cc
+++ parser/prism/Translator.cc
@@ -1815,7 +1815,6 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
             };
 
             enforceHasExpr(receiver);
-            auto supportedCallType = true;
 
             unique_ptr<parser::Node> blockBody;       // e.g. `123` in `foo { |x| 123 }`
             unique_ptr<parser::Node> blockParameters; // e.g. `|x|` in `foo { |x| 123 }`
@@ -1928,7 +1927,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
                         } else {
                             auto blockPassArgNode = translate(bp->expression);
 
-                            if (supportedCallType) {
+                            if (true) {
                                 enforceHasExpr(blockPassArgNode);
 
                                 blockPassArg = blockPassArgNode->takeDesugaredExpr();
@@ -1958,6 +1957,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
                 supportedBlock = true;
             }
 
+            auto supportedCallType = true;
             supportedCallType &= supportedBlock;
 
             // TODO: Direct desugaring support for conditional sends is not implemented yet.
