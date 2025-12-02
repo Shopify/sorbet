2eb820114e0f5d16a3407cd5f758192c9720c2df
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-12-02 12:11:57 -0500

    Remove always-true bool param

--- parser/prism/Translator.cc
+++ parser/prism/Translator.cc
@@ -1854,8 +1854,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
                                                           make_move_iterator(sorbetShadowParams.begin()),
                                                           make_move_iterator(sorbetShadowParams.end()));
 
-                                    std::tie(blockParamsStore, blockStatsStore) =
-                                        desugarParametersNode(params->params, true);
+                                    std::tie(blockParamsStore, blockStatsStore) = desugarParametersNode(params->params);
 
                                     blockParameters = move(params);
                                 }
@@ -2615,7 +2614,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
             ast::MethodDef::PARAMS_store paramsStore;
             ast::InsSeq::STATS_store statsStore;
             if (params != nullptr) {
-                std::tie(paramsStore, statsStore) = desugarParametersNode(params->params, true);
+                std::tie(paramsStore, statsStore) = desugarParametersNode(params->params);
             }
 
             auto methodBody = takeDesugaredExprOrEmptyTree(body);
@@ -4353,12 +4352,7 @@ Translator::translateParametersNode(pm_parameters_node *paramsNode, core::LocOff
     return {make_unique<parser::Params>(location, move(params)), enclosingBlockParamName};
 }
 
-tuple<ast::MethodDef::PARAMS_store, ast::InsSeq::STATS_store>
-Translator::desugarParametersNode(NodeVec &params, bool attemptToDesugarParams) {
-    if (!attemptToDesugarParams) {
-        throw PrismFallback{};
-    }
-
+tuple<ast::MethodDef::PARAMS_store, ast::InsSeq::STATS_store> Translator::desugarParametersNode(NodeVec &params) {
     auto supportedParams = absl::c_all_of(params, [](auto &param) {
         return param->hasDesugaredExpr() ||
                // These other block types don't have their own dedicated desugared
