75081422d40ea6e770cc9cf4949d0c66b92817f3
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-12-02 12:07:29 -0500

    Remove always-true bool return

--- parser/prism/Translator.cc
+++ parser/prism/Translator.cc
@@ -1854,7 +1854,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
                                                           make_move_iterator(sorbetShadowParams.begin()),
                                                           make_move_iterator(sorbetShadowParams.end()));
 
-                                    std::tie(blockParamsStore, blockStatsStore, std::ignore) =
+                                    std::tie(blockParamsStore, blockStatsStore) =
                                         desugarParametersNode(params->params, true);
 
                                     blockParameters = move(params);
@@ -2615,7 +2615,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
             ast::MethodDef::PARAMS_store paramsStore;
             ast::InsSeq::STATS_store statsStore;
             if (params != nullptr) {
-                std::tie(paramsStore, statsStore, std::ignore) = desugarParametersNode(params->params, true);
+                std::tie(paramsStore, statsStore) = desugarParametersNode(params->params, true);
             }
 
             auto methodBody = takeDesugaredExprOrEmptyTree(body);
@@ -4353,7 +4353,7 @@ Translator::translateParametersNode(pm_parameters_node *paramsNode, core::LocOff
     return {make_unique<parser::Params>(location, move(params)), enclosingBlockParamName};
 }
 
-tuple<ast::MethodDef::PARAMS_store, ast::InsSeq::STATS_store, bool /* didDesugarParams */>
+tuple<ast::MethodDef::PARAMS_store, ast::InsSeq::STATS_store>
 Translator::desugarParametersNode(NodeVec &params, bool attemptToDesugarParams) {
     if (!attemptToDesugarParams) {
         throw PrismFallback{};
@@ -4401,7 +4401,7 @@ Translator::desugarParametersNode(NodeVec &params, bool attemptToDesugarParams) 
         }
     }
 
-    return make_tuple(move(paramsStore), move(statsStore), true);
+    return make_tuple(move(paramsStore), move(statsStore));
 }
 
 core::LocOffsets Translator::findItParamUsageLoc(pm_statements_node *statements) {
