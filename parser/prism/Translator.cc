ec14f791f07bb69333e08f12552e92bc652bfc14
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-12-02 11:41:16 -0500

    Remove `attemptToDesugarBlockParams`

--- parser/prism/Translator.cc
+++ parser/prism/Translator.cc
@@ -1805,7 +1805,6 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
 
                     enforceHasExpr(blockBody);
 
-                    auto attemptToDesugarBlockParams = supportedCallType;
                     bool didDesugarBlockParams = false;
 
                     if (blockNode->parameters != nullptr) {
@@ -1837,7 +1836,7 @@ unique_ptr<parser::Node> Translator::translate(pm_node_t *node) {
                                                           make_move_iterator(sorbetShadowParams.end()));
 
                                     std::tie(blockParamsStore, blockStatsStore, didDesugarBlockParams) =
-                                        desugarParametersNode(params->params, attemptToDesugarBlockParams);
+                                        desugarParametersNode(params->params, true);
 
                                     blockParameters = move(params);
                                 }
