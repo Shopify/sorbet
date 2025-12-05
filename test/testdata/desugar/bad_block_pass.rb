71425878b97f6850485fafaf22df9299dbef6271
Alexander Momchilov <alexander.momchilov@shopify.com>
2025-12-05 14:27:39 -0500

    Fix test

--- test/testdata/desugar/bad_block_pass.rb
+++ test/testdata/desugar/bad_block_pass.rb
@@ -5,14 +5,14 @@ def a(...); end
 a[&blk] = 1
 # ^^^^ error: Unsupported block pass node in non-final argument
 a.[]=(&blk, 1)
+#         ^ error: unexpected token ","
 #     ^^^^ error: Unsupported block pass node in non-final argument
-#         ^ error: Unexpected token ","
 a(&blk, 1)
+#     ^ error: unexpected token ","
 # ^^^^ error: Unsupported block pass node in non-final argument
 a.[]=(0, &blk, 1)
+#            ^ error: unexpected token ","
 #        ^^^^ error: Unsupported block pass node in non-final argument
-#            ^ error: Unexpected token ","
-# ^^^^ error: Unsupported block pass node in non-final argument
 a(0, &blk, 1)
+#        ^ error: unexpected token ","
 #    ^^^^ error: Unsupported block pass node in non-final argument
-#        ^ error: Unexpected token ","
