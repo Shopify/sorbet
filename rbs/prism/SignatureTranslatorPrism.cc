58b03c967e03c5852144d488bb820eca496b0e59
Allison Cretel <allison.cretel@shopify.com>
2025-12-23 14:48:46 -0500

    Rename local parser to rbsParser

--- rbs/prism/SignatureTranslatorPrism.cc
+++ rbs/prism/SignatureTranslatorPrism.cc
@@ -19,45 +19,45 @@ Parser makeParser(string_view str) {
 pm_node_t *
 SignatureTranslatorPrism::translateAssertionType(absl::Span<pair<core::LocOffsets, core::NameRef>> typeParams,
                                                  const rbs::RBSDeclaration &assertion) {
-    Parser parser = makeParser(assertion.string);
-    rbs_node_t *rbsType = parser.parseType();
+    Parser rbsParser = makeParser(assertion.string);
+    rbs_node_t *rbsType = rbsParser.parseType();
 
-    if (parser.hasError()) {
-        auto loc = assertion.typeLocFromRange(parser.getError()->token.range);
+    if (rbsParser.hasError()) {
+        auto loc = assertion.typeLocFromRange(rbsParser.getError()->token.range);
         if (auto e = ctx.beginIndexerError(loc, core::errors::Rewriter::RBSSyntaxError)) {
-            e.setHeader("Failed to parse RBS type ({})", parser.getError()->message);
+            e.setHeader("Failed to parse RBS type ({})", rbsParser.getError()->message);
         }
         return nullptr;
     }
 
-    auto typeToParserNodePrism = TypeToParserNodePrism(ctx, typeParams, move(parser), *this->parser);
+    auto typeToParserNodePrism = TypeToParserNodePrism(ctx, typeParams, move(rbsParser), *parser);
     return typeToParserNodePrism.toPrismNode(rbsType, assertion);
 }
 
 pm_node_t *SignatureTranslatorPrism::translateType(const RBSDeclaration &declaration) {
-    Parser parser = makeParser(declaration.string);
-    rbs_node_t *rbsType = parser.parseType();
+    Parser rbsParser = makeParser(declaration.string);
+    rbs_node_t *rbsType = rbsParser.parseType();
 
-    if (parser.hasError()) {
-        auto offset = declaration.typeLocFromRange(parser.getError()->token.range);
+    if (rbsParser.hasError()) {
+        auto offset = declaration.typeLocFromRange(rbsParser.getError()->token.range);
         if (auto e = ctx.beginIndexerError(offset, core::errors::Rewriter::RBSSyntaxError)) {
-            e.setHeader("Failed to parse RBS type ({})", parser.getError()->message);
+            e.setHeader("Failed to parse RBS type ({})", rbsParser.getError()->message);
         }
         return nullptr;
     }
 
-    auto typeTranslator = TypeToParserNodePrism(ctx, {}, move(parser), *this->parser);
+    auto typeTranslator = TypeToParserNodePrism(ctx, {}, move(rbsParser), *parser);
     return typeTranslator.toPrismNode(rbsType, declaration);
 }
 
 pm_node_t *SignatureTranslatorPrism::translateAttrSignature(const pm_call_node_t *call,
                                                             const RBSDeclaration &declaration,
                                                             absl::Span<const Comment> annotations) {
-    Parser parser = makeParser(declaration.string);
-    rbs_node_t *rbsType = parser.parseType();
+    Parser rbsParser = makeParser(declaration.string);
+    rbs_node_t *rbsType = rbsParser.parseType();
 
-    if (parser.hasError()) {
-        auto offset = declaration.typeLocFromRange(parser.getError()->token.range);
+    if (rbsParser.hasError()) {
+        auto offset = declaration.typeLocFromRange(rbsParser.getError()->token.range);
         // First parse failed, let's check if the user mistakenly used a method signature on an accessor
         auto methodParser = makeParser(declaration.string);
         methodParser.parseMethodType();
@@ -75,25 +75,25 @@ pm_node_t *SignatureTranslatorPrism::translateAttrSignature(const pm_call_node_t
         return nullptr;
     }
 
-    auto methodTypeToParserNode = MethodTypeToParserNodePrism(ctx, move(parser), *this->parser);
+    auto methodTypeToParserNode = MethodTypeToParserNodePrism(ctx, move(rbsParser), *parser);
     return methodTypeToParserNode.attrSignature(call, rbsType, declaration, annotations);
 }
 
 pm_node_t *SignatureTranslatorPrism::translateMethodSignature(const pm_node_t *methodDef,
                                                               const RBSDeclaration &declaration,
                                                               absl::Span<const Comment> annotations) {
-    Parser parser = makeParser(declaration.string);
-    rbs_method_type_t *rbsMethodType = parser.parseMethodType();
+    Parser rbsParser = makeParser(declaration.string);
+    rbs_method_type_t *rbsMethodType = rbsParser.parseMethodType();
 
-    if (parser.hasError()) {
-        auto offset = declaration.typeLocFromRange(parser.getError()->token.range);
+    if (rbsParser.hasError()) {
+        auto offset = declaration.typeLocFromRange(rbsParser.getError()->token.range);
         if (auto e = ctx.beginIndexerError(offset, core::errors::Rewriter::RBSSyntaxError)) {
-            e.setHeader("Failed to parse RBS signature ({})", parser.getError()->message);
+            e.setHeader("Failed to parse RBS signature ({})", rbsParser.getError()->message);
         }
         return nullptr;
     }
 
-    auto methodTypeToParserNodePrism = MethodTypeToParserNodePrism(ctx, move(parser), *this->parser);
+    auto methodTypeToParserNodePrism = MethodTypeToParserNodePrism(ctx, move(rbsParser), *parser);
     return methodTypeToParserNodePrism.methodSignature(methodDef, rbsMethodType, declaration, annotations);
 }
 
